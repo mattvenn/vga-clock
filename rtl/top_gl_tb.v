db029c38607c8d27f54b48bd8fd423cae3c4ed3d
Matt Venn <matt@mattvenn.net>
2021-02-04 11:58:48 +0100

    ensure all inputs are 0

--- rtl/top_gl_tb.v
+++ rtl/top_gl_tb.v
@@ -9,7 +9,9 @@ module test;
     reg clk = 0;
     reg px_clk = 0;
     reg reset_n = 0;
-    reg adj_sec = 0;
+    reg adj_sec = 1'b0;
+    reg adj_min = 1'b0;
+    reg adj_hrs = 1'b0;
     localparam DAY = 60 * 60 * 60;
 
     initial begin
@@ -21,7 +23,7 @@ module test;
         $finish;
     end
 
-    vga_clock top_0(.VGND(1'b0), .VPWR(1'b1), .clk(px_clk), .reset_n(reset_n), .adj_sec(adj_sec));
+    vga_clock top_0(.VGND(1'b0), .VPWR(1'b1), .clk(px_clk), .reset_n(reset_n), .adj_sec(adj_sec), .adj_min(adj_min), .adj_hrs(adj_hrs));
     always #4 clk = !clk;
     always #1 px_clk = !px_clk;
 
